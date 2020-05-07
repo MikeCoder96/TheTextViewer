0749b2bc24495a699daa1eb881304664ec2599c5
redin0s <redin0s@localhost.localdomain>
2020-05-07 23:15:17 +0200

    Added formpackage folder with darkmode style and main form
    [FIXED]

--- src/mainpackage/Main.java
+++ src/mainpackage/Main.java
@@ -1,20 +1,29 @@
 package mainpackage;
 	
+import java.io.File;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/formpackage/mainform.fxml"));
+	        AnchorPane root = (AnchorPane) loader.load();
+	        Scene scene = new Scene(root, 800, 800);
+	        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+	        primaryStage.initStyle(StageStyle.UNDECORATED);
+	        primaryStage.setScene(scene);        
+	        primaryStage.setTitle("Prova");
+	        primaryStage.setMinHeight(400);
+	        primaryStage.setMinWidth(400);
+	        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
