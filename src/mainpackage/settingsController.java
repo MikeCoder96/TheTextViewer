17b1ecca019c706abf7d7e2c3d4f204cebeff57f
MikeCoder96 <bncmhl96r23a773d@studenti.unical.it>
2020-05-08 15:16:03 +0200

    SettingsForm
    Fully Working settingsForm

--- src/mainpackage/settingsController.java
+++ src/mainpackage/settingsController.java
@@ -8,33 +8,43 @@ import utilspackage.utils;
 
 public class settingsController {
 
-	@FXML
-	private RadioButton radioButton1;
-	
-	@FXML
-	private RadioButton radioButton2;
-	
-    private ToggleGroup group = new ToggleGroup();
+    @FXML
+    private RadioButton radioButton1;
+
+    @FXML
+    private RadioButton radioButton2;
     
     @FXML
-    void initialize() {
-    	radioButton1.setToggleGroup(group);
-    	radioButton2.setToggleGroup(group);
-    	radioButton1.setSelected(true);
+    private void initialize() {
+    	//TO-DO: reading theme from file settings
     }
     
     @FXML
     void setDarkMode(ActionEvent event) {
-        utils.actualScene.getStylesheets().remove(utils.theme2Url);
-        if(!utils.actualScene.getStylesheets().contains(utils.theme1Url))
-        	utils.actualScene.getStylesheets().add(utils.theme1Url);
-
+        sceneHandler.getInstance().getMainScene().getStylesheets().remove(utils.theme2Url);
+        if(!sceneHandler.getInstance().getMainScene().getStylesheets().contains(utils.theme1Url))
+        	sceneHandler.getInstance().getMainScene().getStylesheets().add(utils.theme1Url);
+        
+        sceneHandler.getInstance().getSettingsScene().getStylesheets().remove(utils.theme2Url);
+        if(!sceneHandler.getInstance().getSettingsScene().getStylesheets().contains(utils.theme1Url))
+        	sceneHandler.getInstance().getSettingsScene().getStylesheets().add(utils.theme1Url);
+        
+        radioButton1.setSelected(true);
+        radioButton2.setSelected(false);
     }
 
     @FXML
     void setWhiteMode(ActionEvent event) {
-    	utils.actualScene.getStylesheets().remove(utils.theme1Url);
-        if(!utils.actualScene.getStylesheets().contains(utils.theme2Url))
-        	utils.actualScene.getStylesheets().add(utils.theme2Url);
+    	sceneHandler.getInstance().getMainScene().getStylesheets().remove(utils.theme1Url);
+        if(!sceneHandler.getInstance().getMainScene().getStylesheets().contains(utils.theme2Url))
+        	sceneHandler.getInstance().getMainScene().getStylesheets().add(utils.theme2Url);
+        
+    	sceneHandler.getInstance().getSettingsScene().getStylesheets().remove(utils.theme1Url);
+        if(!sceneHandler.getInstance().getSettingsScene().getStylesheets().contains(utils.theme2Url))
+        	sceneHandler.getInstance().getSettingsScene().getStylesheets().add(utils.theme2Url);
+       
+        radioButton2.setSelected(true);
+        radioButton1.setSelected(false);
     }
+
 }
