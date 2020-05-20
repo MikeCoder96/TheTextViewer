eadaaf6135472e698b27f888bc8d7310109dabcc
MikeCoder96 <bncmhl96r23a773d@studenti.unical.it>
2020-05-20 11:53:23 +0200

    Added dynamic file reader

--- src/mainpackage/MainController.java
+++ src/mainpackage/MainController.java
@@ -1,38 +1,50 @@
 package mainpackage;
 
-import java.util.ArrayList;
-
+import java.io.File;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import utilspackage.CellFactoryAdvanced;
 import utilspackage.TreeCategory;
+import utilspackage.Utils;
 
 public class MainController {
-	
-	private ArrayList<String> books = new ArrayList<String>();
+
 	private TreeItem<String> rootItem;
 
-    @FXML
-    private ContextMenu contextMenu1;
-    @FXML
+	@FXML
+	private ContextMenu contextMenu1;
+	@FXML
 	private TreeView<String> treeView1;
-    @FXML
-    private AnchorPane textContentPane;
+	@FXML
+	private AnchorPane textContentPane;
 	@FXML
 	private SplitPane splitPane1;
 
 	@FXML
 	private MenuItem settingsButton;
 
-    @FXML
-    private MenuItem menuItem1;
+	@FXML
+	private MenuItem menuItem1;
+
+	@FXML
+	void onOpenFile(ActionEvent event) {
+		FileChooser fc = new FileChooser();
+		File file = fc.showOpenDialog(null);
+		if (file != null) {
+			addBooks(file.getName(), file);
+			System.out.println(file.getPath());
+		}
+	}
 
 	@FXML
 	void initialize() {
@@ -41,45 +53,29 @@ public class MainController {
 		treeView1.setShowRoot(false);
 		treeView1.setEditable(true);
 		treeView1.setCellFactory(new CellFactoryAdvanced());
-		
-		
-		
-		addBooks("a");
-		addBooks("B");
-		addBooks("c");
-		addBooks("D");
-		addBooks("t");
-		addBooks("w");
-		addBooks("e");
-		/*addBooks("r");
-		addBooks("y");
-		addBooks("u");
-		addBooks("i");
-		addBooks("o");
-		addBooks("p");
-		addBooks("1");
-		addBooks("2");
-		addBooks("3");
-		addBooks("4");
-		addBooks("5");
-		addBooks("6");
-		addBooks("7");
-		addBooks("8");
-		addBooks("9");
-		*/
-
-		showBooksToTreeView();
+		treeView1.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> 
+		{
+			OpenText(newValue.getValue());
+			System.out.println("Selected Text : " + newValue.getValue());
+		});
 	}
 
-	public void addBooks(String Title) {
-		books.add(Title);
-	}
-
-	void showBooksToTreeView() {
-		for (int i = 0; i < books.size(); i++) {
-			TreeItem<String> item = new TreeItem<String>(books.get(i));
+	public void addBooks(String Title, File file) {
+		Boolean res = Utils.addBooks(Title, file);
+		
+		if (res)
+		{
+			TreeItem<String> item = new TreeItem<String>(Title);
 			rootItem.getChildren().add(item);
 		}
+		else
+		{
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Book Already Exist!");
+			alert.setHeaderText("The book: " + Title + " is already present in your library!");
+			alert.showAndWait();
+		}
+
 	}
 
 	@FXML
@@ -92,22 +88,23 @@ public class MainController {
 		TreeCategory prova = new TreeCategory("Inserisci nome");
 		rootItem.getChildren().add(prova);
 	}
-	
-	@FXML
-    void tempOpenText(ActionEvent event) {
-    	FileChooser fc = new FileChooser();
-    	try {
-    		TextFileViewer tfv = new TextFileViewer(fc.showOpenDialog(null));
-    		textContentPane.getChildren().clear();
-    		//set to fill AnchorPane
-    		AnchorPane.setBottomAnchor(tfv, 0.0);
-    		AnchorPane.setLeftAnchor(tfv, 0.0);
-    		AnchorPane.setTopAnchor(tfv, 0.0);
-    		AnchorPane.setRightAnchor(tfv, 0.0);
-    		textContentPane.getChildren().add(tfv);
-    		
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-    }
+
+	void OpenText(String title) {
+		try {
+			File toShow = Utils.getBookFromTitle(title);
+			if (toShow == null)
+				return;
+			TextFileViewer tfv = new TextFileViewer(toShow);
+			textContentPane.getChildren().clear();
+			// set to fill AnchorPane
+			AnchorPane.setBottomAnchor(tfv, 0.0);
+			AnchorPane.setLeftAnchor(tfv, 0.0);
+			AnchorPane.setTopAnchor(tfv, 0.0);
+			AnchorPane.setRightAnchor(tfv, 0.0);
+			textContentPane.getChildren().add(tfv);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/utilspackage/Utils.java
+++ src/utilspackage/Utils.java
@@ -1,7 +1,24 @@
 package utilspackage;
 
+import java.io.File;
+import java.util.HashMap;
+
 public class Utils {
 	final public static String DARKTHEME = "/formpackage/style_dark.css";
 	final public static String WHITETHEME = "/formpackage/style_white.css";
+	public static HashMap<String, File> books = new HashMap<String, File>();
 	
+	public static Boolean addBooks(String title, File path) {	
+		if (books.containsKey(title))
+			return false;
+		else
+		{
+			books.put(title, path);
+			return true; 
+		}
+	}
+	
+	public static File getBookFromTitle(String title) {
+		return books.get(title);
+	}
 }
